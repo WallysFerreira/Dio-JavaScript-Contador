3c103c32763beb5108a1cfc12db589c3952debc9
WallysFerreira <wallysfsilva@gmail.com>
2022-06-23 11:55:12 -0300

    add increment and decrement by 10 feature

--- script.js
+++ script.js
@@ -1,49 +1,71 @@
-var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
-var subtrairButton = document.getElementsByName('subtrair')[0];
+
+var currentNumberWrapper = document.getElementById('currentNumber');
+
+var subtrair10Button = document.getElementsByName('subtrair')[0];
+var subtrairButton = document.getElementsByName('subtrair')[1];
+
 var adicionarButton = document.getElementsByName('adicionar')[0];
+var adicionar10Button = document.getElementsByName('adicionar')[1];
 
 adicionarButton.addEventListener('click', increment);
+adicionar10Button.addEventListener('click', increment10);
 subtrairButton.addEventListener('click', decrement);
+subtrair10Button.addEventListener('click', decrement10);
+
 
 function increment() {
   currentNumber++;
   currentNumberWrapper.innerHTML = currentNumber;
 
+  changeColor();
+  hideButtons();
+}
 
-  if (currentNumber < 0) {
-    currentNumberWrapper.style.color = 'red';
-  } else {
-    currentNumberWrapper.style.color = 'black';
-  }
+function increment10() {
+  currentNumber = currentNumber + 10;
+  currentNumberWrapper.innerHTML = currentNumber;
 
-  if (currentNumber < -10) {
-    subtrairButton.classList.add('hidden');
-  } else if (currentNumber > 10) {
-    adicionarButton.classList.add('hidden');
-  } else {
-    subtrairButton.classList.remove('hidden');
-    adicionarButton.classList.remove('hidden');
-  }
+  changeColor();
+  hideButtons();
 }
 
 function decrement() {
   currentNumber--;
   currentNumberWrapper.innerHTML = currentNumber;
 
+  changeColor();
+  hideButtons();
+}
+
+function decrement10() {
+  currentNumber = currentNumber - 10;
+  currentNumberWrapper.innerHTML = currentNumber;
+
+  changeColor();
+  hideButtons();
+}
+
+function hideButtons() {
+  if (currentNumber < -100) {
+    for (i = 0; i < document.getElementsByName('subtrair').length; i++) {
+      document.getElementsByName('subtrair')[i].classList.add('hidden');
+    }
+  } else if (currentNumber > 100) {
+    for (i = 0; i < document.getElementsByName('adicionar').length; i++) {
+      document.getElementsByName('adicionar')[i].classList.add('hidden');
+    }
+  } else {
+    for (i = 0; i < document.getElementsByTagName('button').length; i++) {
+      document.getElementsByTagName('button')[i].classList.remove('hidden');
+    }
+  }
+}
+
+function changeColor() {
   if (currentNumber < 0) {
     currentNumberWrapper.style.color = 'red';
   } else {
     currentNumberWrapper.style.color = 'black';
   }
-
-  if (currentNumber < -10) {
-    subtrairButton.classList.add('hidden');
-  } else if (currentNumber > 10) {
-    adicionarButton.classList.add('hidden');
-  } else {
-    subtrairButton.classList.remove('hidden');
-    adicionarButton.classList.remove('hidden');
-  }
-
 }
