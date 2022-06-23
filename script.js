76c8599e964fd977485e81a31b677c8f3e9ff9ef
WallysFerreira <wallysfsilva@gmail.com>
2022-06-23 11:25:38 -0300

    red text when number is negative

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ var adicionarButton = document.getElementsByName('adicionar')[0];
 adicionarButton.addEventListener('click', increment);
 subtrairButton.addEventListener('click', decrement);
 
-
 function increment() {
   currentNumber++;
-  currentNumberWrapper.innerHTML = currentNumber;
+  currentNumberWrapper.innerHTML = currentNumber;if (currentNumber < 0) {
+    currentNumberWrapper.style.color = 'red';
+  } else {
+    currentNumberWrapper.style.color = 'black';
+  }
 }
 
 function decrement() {
   currentNumber--;
   currentNumberWrapper.innerHTML = currentNumber;
+  if (currentNumber < 0) {
+    currentNumberWrapper.style.color = 'red';
+  } else {
+    currentNumberWrapper.style.color = 'black';
+  }
 }
