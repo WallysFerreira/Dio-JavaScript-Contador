3c84d96b216d4b47fad7b5fc1c73742a63bf55ad
WallysFerreira <wallysfsilva@gmail.com>
2022-06-23 11:05:48 -0300

    use event listeners

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
+var subtrairButton = document.getElementsByName('subtrair')[0];
+var adicionarButton = document.getElementsByName('adicionar')[0];
+
+adicionarButton.addEventListener('click', increment);
+subtrairButton.addEventListener('click', decrement);
 
 
 function increment() {
